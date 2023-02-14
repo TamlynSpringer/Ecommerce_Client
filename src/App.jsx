18d1c1a3cca3d4c84c41b84016d25e65f8db7039
TamlynSpringer <tamlynspringer@gmail.com>
2023-02-14 21:42:48 +0100

    Added styled components with react-boptstrap to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,35 @@ import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap'
 
 const App = () => {
 return (
-    <>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to='/'>Baltic Store</Link>      
+        <Navbar bg='dark' variant='dark'>
+          <Container>
+            <LinkContainer to='/'>
+            <Navbar.Brand>Baltic Store</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main className='main'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/product/:slug' element={<Product />} />
-        </Routes>       
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product/:slug' element={<Product />} />
+          </Routes>       
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>Tamlyn Springer | 2023 | All rights reserved</div>
+      </footer>
      
-    </>
+    </div>
   )
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,66 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from '../api/axios';
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false }
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default: 
+      return state;
+  }
+};
+
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [errMsg, setErrMsg] = useState('');
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, { 
+    products: [],
+    loading: true, 
+    error: '' 
+  });
+
   const errRef = useRef();
 
-  const fetchData = async () => {
-    try {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg('No server response');
-      }  else {
-          setErrMsg('Fetch failed')
-        }
-        errRef.current.focus();
+  useEffect(() => {    
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message })
       }
     };
-  
-  useEffect(() => {    
     fetchData();
   }, []);
 
   return (
     <>
-      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
       <h2>Featured products</h2>
       <section className='products'>
-        {products?.map(product => (
-          <article className='product' key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <Link to={`/product/${product.slug}`}>
-              <p>{product.name}</p>
-            </Link>            
-            <p><strong>{product.price}</strong></p>
-            <button>Add to cart</button>
-          </article>))
-        }
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p ref={errRef} className={error ? "errmsg" : "offscreen"} aria-live="assertive">{error}</p>
+        ) : (
+          products?.map(product => (
+            <article className='product' key={product.slug}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>            
+              <p><strong>{product.price}</strong></p>
+              <button>Add to cart</button>
+            </article>))          
+        )}
       </section>
     </>
   )
-}
+};
 
-export default Home
+export default Home;
