f8970e4cd2c78e6e03c3fdf5ee6c38889d8c4652
TamlynSpringer <tamlynspringer@gmail.com>
2023-03-02 17:12:43 +0100

    Added host API link to axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000'
+const baseURL = 'http://localhost:8000' || 'https://baltic-store-api.cyclic.app'
 
 export default axios.create({
   baseURL: baseURL
